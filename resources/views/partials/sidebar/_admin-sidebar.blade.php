c3b4e1b61f43de5309be39fe70263b74f9f965a7
Gbudu Joseph <talktojoegee@gmail.com>
2020-08-24 16:19:23 +0100

    Dev branch

--- resources/views/partials/sidebar/_admin-sidebar.blade.php
+++ resources/views/partials/sidebar/_admin-sidebar.blade.php
@@ -425,6 +425,11 @@
                         </a>
                     </li>
                     <li class=" ">
+                        <a href="{{route('tenants')}}">
+                            <span class="pcoded-mtext">Subscription</span>
+                        </a>
+                    </li>
+                    <li class=" ">
                         <a href="{{route('plans-n-features')}}">
                             <span class="pcoded-mtext">Plans & Features</span>
                         </a>
