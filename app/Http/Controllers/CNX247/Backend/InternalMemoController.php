8d5f0e4f8cc3aa0901e4f32cdb913aac9e3b2595
Gbudu Joseph <talktojoegee@gmail.com>
2020-09-19 15:56:12 +0100

    Recent changes

--- app/Http/Controllers/CNX247/Backend/InternalMemoController.php
+++ app/Http/Controllers/CNX247/Backend/InternalMemoController.php
@@ -24,7 +24,8 @@ class InternalMemoController extends Controller
     * Get list of all employees
     */
     public function index(){
-        return view('backend.workflow.memo.internal-memo');
+        $users = User::where('tenant_id', Auth::user()->tenant_id)->orderBy('first_name', 'ASC')->get();
+        return view('backend.workflow.memo.internal-memo',['users'=>$users]);
     }
 
     public function store(Request $request){
@@ -38,7 +39,7 @@ class InternalMemoController extends Controller
             $extension = $request->file('attachment');
             $extension = $request->file('attachment')->getClientOriginalExtension();
             $size = $request->file('attachment')->getSize();
-            $dir = 'assets/uploads/requisition/';
+            $dir = 'assets/uploads/attachments/';
             $filename = uniqid().'_'.time().'_'.date('Ymd').'.'.$extension;
             $request->file('attachment')->move(public_path($dir), $filename);
         }else{
@@ -70,7 +71,18 @@ class InternalMemoController extends Controller
             $resp->post_id = $id;
             $resp->tenant_id = Auth::user()->tenant_id;
             $resp->save();
-        }else{
+        }elseif($request->to == 2){ //specific
+            foreach($request->employees as $person){
+                 $part = new ResponsiblePerson;
+                 $part->post_id = $id;
+                 $part->user_id = $person;
+                 $part->tenant_id = Auth::user()->tenant_id;
+                 $part->save();
+                 //send notification
+                 $user = User::find($person);
+                 $user->notify(new NewPostNotification($memo));
+             }
+        }else{ //1=depart
             $users = User::where('department_id', $request->department)
                             ->where('tenant_id', Auth::user()->tenant_id)
                             ->get();
@@ -95,7 +107,7 @@ class InternalMemoController extends Controller
                 $message = "<strong>Success!</strong> There're no employees in this department. Memo sent to all employees instead";
             }
         }
-        session()->flash("success", $message);
+        session()->flash("success", "<strong>Success!</strong> Internal memo sent.");
         return back();
     }
 
