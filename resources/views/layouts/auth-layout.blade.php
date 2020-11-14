5d0c43863ceb1d2478d630dbde455d76f91fe0bb
cjamanambu <oamanambu@yahoo.com>
2020-11-14 11:16:46 +0100

    Login page

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -1,62 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>CNX247 | @yield('title')</title>
-    <!-- Meta -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Landing page template for creative dashboard">
-    <meta name="keywords" content="Landing page template">
-    <!-- Favicon icon -->
-    <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
-    <!-- Google font--><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
-    <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="/assets/bower_components/bootstrap/css/bootstrap.min.css">
-    <!-- themify-icons line icon -->
-    <link rel="stylesheet" type="text/css" href="/assets/icon/themify-icons/themify-icons.css">
-    <!-- ico font -->
-    <link rel="stylesheet" type="text/css" href="/assets/icon/icofont/css/icofont.css">
-    <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
-    @yield('extra-styles')
-    @livewireStyles
-</head>
-
+@include('partials.frontend._header')
 <body>
+<div id="preloader">
+	<div id="status">
+		<div class="spinner">
+			<div class="double-bounce1"></div>
+			<div class="double-bounce2"></div>
+		</div>
+	</div>
+</div>
 
-@yield('main-content')
+@yield('content')
 
+<script src="{{asset('/frontend/js/jquery-3.5.1.min.js')}}"></script>
+<script src="{{asset('/frontend/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('/frontend/js/jquery.easing.min.js')}}"></script>
+<script src="{{asset('/frontend/js/scrollspy.min.js')}}"></script>
+<script src="{{asset('/frontend/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('/frontend/js/owl.init.js')}}"></script>
+<script src="{{asset('/frontend/js/feather.min.js')}}"></script>
+<script src="{{asset('/frontend/js/swiper.min.js')}}"></script>
+<script src="{{asset('/frontend/js/swiper.init.js')}}"></script>
+<script src="{{asset('/frontend/js/jquery.flexslider-min.js')}}"></script>
+<script src="{{asset('/frontend/js/flexslider.init.js')}}"></script>
+<script src="https://unicons.iconscout.com/release/v2.1.9/script/monochrome/bundle.js"></script>
 
-
-    <script type="text/javascript" src="/assets/bower_components/jquery/js/jquery.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/popper.js/js/popper.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/bootstrap/js/bootstrap.min.js"></script>
-    <!-- jquery slimscroll js -->
-    <script type="text/javascript" src="/assets/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
-    <!-- modernizr js -->
-    <script type="text/javascript" src="/assets/bower_components/modernizr/js/modernizr.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/modernizr/js/css-scrollbars.js"></script>
-    <!-- i18next.min.js -->
-    <script type="text/javascript" src="/assets/bower_components/i18next/js/i18next.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
-    <script type="text/javascript" src="/assets/bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
-    <script type="text/javascript" src="/assets/js/common-pages.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      
-      gtag('config', 'UA-23581568-13');
-    </script>
-
-    @yield('extra-scripts')
-    
-  @livewireScripts
+<script type="text/javascript" src="{{asset('/assets/js/cus/axios.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/assets/js/cus/notify.js')}}"></script>
+@livewireScripts
+@yield('extra-scripts')
+<script src="{{asset('/frontend/js/app.js')}}"></script>
 </body>
-
 </html>
