b4a86469757181ed828d8e9e6716ab3c6a223a04
Joseph <talktojoegee@gmail.com>
2021-01-04 10:50:55 +0100

    Filter list contacts: only employees whose account status is active reflect

--- app/Http/Controllers/CNX247/Backend/ChatnCallsController.php
+++ app/Http/Controllers/CNX247/Backend/ChatnCallsController.php
@@ -179,5 +179,159 @@ class ChatnCallsController extends Controller
         }
 
         return $response;
-    }
+		}
+
+
+	/* 	public function chat(){
+			return view('backend.chat.view.chat');
+		} */
+
+		public function initializeChat(){
+			// get all users except the authenticated one
+			$users = User::where('id', '!=', auth()->id())->where('account_status', 1)->where('tenant_id', Auth::user()->tenant_id)->get();
+			$unreadIds = Message::select(\DB::raw('`from_id` as sender_id, count(`from_id`) as unread'))
+            ->where('to_id', auth()->id())
+						->where('is_read', 0)
+						->where('tenant_id', Auth::user()->tenant_id)
+            ->groupBy('from_id')
+						->get();
+			$users = $users->map(function($user) use ($unreadIds) {
+							$userUnread = $unreadIds->where('sender_id', $user->id)->first();
+
+							$user->unread = $userUnread ? $userUnread->unread : 0;
+
+							return $user;
+					});
+
+			$auth_user = Auth::user();
+			return response()->json(['users'=>$users, 'auth_user'=>$auth_user],200);
+		}
+
+
+		public function chatWith($id){
+			$my_id = Auth::user()->id;
+			Message::where('from_id', $id)->where('to_id', $my_id)->update(['is_read' => 1]);
+			$messages = Message::where(function($q) use ($id) {
+						$q->where('from_id', Auth::user()->id);
+						$q->where('to_id', $id);
+						$q->where('status', 0); //not cleared messages
+						})->orWhere(function($q) use ($id) {
+								$q->where('from_id', $id);
+								$q->where('to_id', Auth::user()->id);
+						})
+						->get();
+			$auth_user = Auth::user();
+			$selected_user = User::where('tenant_id', Auth::user()->tenant_id)->where('id', $id)->first();
+			return response()->json(['messages'=>$messages, 'auth_user'=>$auth_user, 'selected_user'=>$selected_user],200);
+		}
+
+
+		public function clearMessages($id){
+			$my_id = Auth::user()->id;
+			$messages = Message::where(function ($query) use ($id, $my_id) {
+											$query->where('from_id', $id)->where('to_id', $my_id);
+									})->oRwhere(function ($query) use ($id, $my_id) {
+											$query->where('from_id', $my_id)->where('to_id', $id);
+									})->get();
+			foreach($messages as $message){
+				$message->status = 1; //cleared
+				$message->save();
+			}
+
+			return response()->json(['message'=>'Done!'], 200);
+		}
+
+		public function filterContact($search){
+			//$users = User::where('id', '!=', auth()->id())->where('tenant_id', Auth::user()->tenant_id)->get();
+			$filtered_users = User::where('tenant_id', Auth::user()->tenant_id)
+														->where('id', '!=', auth()->id())
+														->where('first_name', 'like', "%{$search}%")
+														->get();
+
+				$auth_user = Auth::user();
+				return response()->json(['users'=>$filtered_users, 'auth_user'=>$auth_user],200);
+
+
+
+		}
+
+
+
+
+
+		public function pushtoToken($token, $title, $body, $userId, $tenantId)
+    {
+        //$token, $title, $body, $userId, $tenantId
+
+        $ch = curl_init("https://fcm.googleapis.com/fcm/send");
+
+        $data = array("clickaction" => "FLUTTERNOTIFICATIONCLICK", "user" => $userId, "tenant_id" => $tenantId);
+
+        //Creating the notification array.
+        $notification = array('title' => $title, 'body' => $body);
+
+        //This array contains, the token and the notification. The 'to' attribute stores the token.
+        $arrayToSend = array('to' =>$token, 'notification' => $notification, 'data' => $data);
+
+        //Generating JSON encoded string form the above array.
+        $json = json_encode($arrayToSend);
+
+        $url = "https://fcm.googleapis.com/fcm/send";
+        //Setup headers:
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: key=AAAAQ6WOcsM:APA91bGx5qqTvsZoFYEMdLiNuM-DlH509sszesHzH5IdW-_OqyRNAw8UrT1VfimR0ITKpF4sJCK7GOoeI0zPYvhkQu4gmow783ZG77Qrj8seV_0QgWkkCBGZ7oSSzdVoTKIckOusTI8x';
+
+        //Setup curl, add headers and post parameters.
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+       // print($result);
+
+        //Send the request
+        //curl_exec($ch);
+
+        //Close request
+        curl_close($ch);
+		}
+
+
+
+		public function ToAllUsers($tenant_id, $title, $body, $userId="32")
+		{
+			$token = "/topics/all";
+			$this->pushtoToken($token, $title, $body, $userId, $tenant_id);
+		}
+
+
+		public function ToSpecificUser($tenant_id, $title, $body, $userId)
+		{
+			$users = User::where('users.tenant_id', $tenant_id)->where('users.id', $userId)->get();
+			foreach($users as $user)
+			{
+					 $token= $user['device_token'];
+					 if($token !=null && !empty($token))
+						{
+							$this->pushtoToken($token, $title, $body, $userId, $tenant_id);
+						}
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
 }
